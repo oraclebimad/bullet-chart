bda1e6ff3823e48bb112dc6f4e76d701f75d658b
Sergio Medina <sergio.medina@oracle.com>
2015-07-14 16:54:05 -0500

    Added plugin description

--- BulletChart.js
+++ BulletChart.js
@@ -3,6 +3,7 @@
   component: {
     'name': 'Bullet Chart',
     'tooltip': 'Designed by Stephen Few, a bullet chart “provides a rich display of data in a small space.” A variation on a bar chart, bullet charts compare a given quantitative measure (such as profit or revenue) against qualitative ranges (e.g., poor, satisfactory, good) and related markers (e.g., the same measure a year ago).',
+    'description': 'Designed by Stephen Few, a bullet chart “provides a rich display of data in a small space.” A variation on a bar chart, bullet charts compare a given quantitative measure (such as profit or revenue) against qualitative ranges (e.g., poor, satisfactory, good) and related markers (e.g., the same measure a year ago).',
     'cssClass': 'bullet-chart-plugin',
     'icon': 'asset://official-plugin.png'
   },
